096658ac122ba11f906c487fa7296ffbc8e66073
Quinn Millican <quinn.millican@tpginc.com>
2023-01-21 11:01:15 -0600

    correct string parsing

--- xtractr.go
+++ xtractr.go
@@ -90,17 +90,42 @@ func unmarshal(request *http.Request, str reflect.Value, pathParams map[string]s
 				}
 				str.Elem().Field(i).SetInt(int64(v))
 			case reflect.Float32, reflect.Float64:
-				v, err := strconv.Atoi(vals[0])
+				bz := 32
+				if field.Type.Kind() == reflect.Float64 {
+					bz = 64
+				}
+				v, err := strconv.ParseFloat(vals[0], bz)
 				if err != nil {
 					return err
 				}
-				str.Elem().Field(i).SetFloat(float64(v))
+				str.Elem().Field(i).SetFloat(v)
+			case reflect.Complex64, reflect.Complex128:
+				bz := 64
+				if field.Type.Kind() == reflect.Complex128 {
+					bz = 128
+				}
+				v, err := strconv.ParseComplex(vals[0], bz)
+				if err != nil {
+					return err
+				}
+				str.Elem().Field(i).SetComplex(v)
 			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-				v, err := strconv.Atoi(vals[0])
+				bz := 0
+				switch field.Type.Kind() {
+				case reflect.Uint8:
+					bz = 8
+				case reflect.Uint16:
+					bz = 16
+				case reflect.Uint32:
+					bz = 32
+				case reflect.Uint64:
+					bz = 64
+				}
+				v, err := strconv.ParseUint(vals[0], 10, bz)
 				if err != nil {
 					return err
 				}
-				str.Elem().Field(i).SetUint(uint64(v))
+				str.Elem().Field(i).SetUint(v)
 			case reflect.Array, reflect.Slice:
 				str.Elem().Field(i).Set(reflect.ValueOf(vals))
 			}
